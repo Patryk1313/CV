ab3dafdc0e2ad4aa216151dc33f737a98254c726
Patryk Kowalik <pacio.2002@go2.pl>
2022-02-14 17:51:00 +0100

    CV v0.02

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector('#btnMenu');
+const nav = document.querySelector('.nav-ul');
+
+btnMenu.addEventListener('click', () => {
+    nav.classList.toggle('show');
+})
