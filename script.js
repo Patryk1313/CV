75b8668bc244ec2844ac7ea9cb92dc7d84068c2b
Patryk Kowalik <pacio.2002@go2.pl>
2022-02-15 20:54:54 +0100

    CV version 0.07

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const btnMenu = document.querySelector('#btnMenu')
 const nav = document.querySelector('.nav-ul')
 const copyBtn = document.querySelector('.btn-copy')
 const copyText = document.querySelector('#copyEmail')
-
 // Responsive Menu
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle('show')
 })
-
 // Copy Clipboard
 copyBtn.addEventListener('click', () => {
     copyText.select();
