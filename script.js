9d4158ff273671157cb28b6ae53ce712dbeb5e8d
Patryk Kowalik <pacio.2002@go2.pl>
2022-02-16 16:38:39 +0100

    Bug fixes and improvements!

--- script.js
+++ script.js
@@ -2,12 +2,42 @@ const btnMenu = document.querySelector('#btnMenu')
 const nav = document.querySelector('.nav-ul')
 const copyBtn = document.querySelector('.btn-copy')
 const copyText = document.querySelector('#copyEmail')
-
 // Responsive Menu
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle('show')
 })
-// Copy Clipboard
+
+//Alert
+    let alertInfo = document.createElement('div')
+    let closeBtn = document.createElement('button')
+
+    closeBtn.classList.add('closeBtn')
+    closeBtn.innerHTML = '<i class="bi bi-x-circle"></i>'
+
+    alertInfo.classList.add('alertInfo')
+    alertInfo.innerText = "Jeżeli widzisz błędy na stronie poinformuj mnie!"
+
+    const body = document.querySelector('body');
+    body.prepend(alertInfo)
+
+    const x = document.querySelector('.alertInfo')
+    x.appendChild(closeBtn)
+
+
+
+closeBtn.addEventListener('click', () => {
+    document.body.removeChild(x)
+})
+
+function show() {
+    const di = document.querySelector('.alertInfo')
+    di.classList.add('show');
+}
+
+setTimeout(show, 5000);
+
+
+
 copyBtn.addEventListener('click', () => {
     copyText.select();
     document.execCommand('copy')
